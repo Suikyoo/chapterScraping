609408568522aa55874c85f5b7728cc266a0b746
Suikyoo <judeanthony02sayson@gmail.com>
2021-07-04 13:52:46 +0800

    automatic chapterScraping unto docx file

--- chapterScraping.py
+++ chapterScraping.py
@@ -1,23 +1,22 @@
+
 import requests
 from bs4 import BeautifulSoup
+import docx
 
-filePath = 'C:\\Users\\dario\\Desktop\\epub.txt'
-chapter = 914
-chapterGoal = 1315
-f = open(filePath, 'a', encoding='utf-8')
+filePath = 'C:\\Users\\dario\\Desktop\\epub.docx'
+chapter = 1
+chapterGoal = 1266
+doc = docx.Document()
 while chapter <= chapterGoal:
-    link = 'https://www.novels.pl/novel/A-Will-Eternal/{}/Chapter-{}-The-Unpredictable-Human-Heart.html'.format(chapter,
-                                                                                                                chapter)
+    link = 'https://bestlightnovel.com/novel_888154455/chapter_{}'.format(chapter)
     website = requests.get(link)
     text = BeautifulSoup(website.content, 'html.parser')
-    chapterText = text.find('div', {'class': 'panel-body article'})
+    chapterText = text.find('div', {'class' : 'vung_doc'})
     chapterContent = chapterText.find_all('p')
-    chapterTitle = chapterText.find('h4')
-    f.write('{}\n\n'.format(chapterTitle.get_text()))
-    # f.write('Chapter {}\n'.format(chapter))
+    doc.add_heading('Chapter {}\n'.format(chapter), 1)
+    #f.write('Chapter {}\n'.format(chapter))
     for tag in chapterContent:
-        f.write('{}\n\n'.format(tag.get_text()))
-    f.write('\n')
+        doc.add_paragraph('   {}\n'.format(tag.get_text()))
     print(chapter)
     chapter += 1
-f.close()
+doc.save(filePath)
